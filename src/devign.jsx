b5b23d2e111a62b44d8628fa806e953c92faa6ff
Tony Rizko <tony@rizkocircle.com>
2016-06-27 01:03:42 -0700

    Create root app component

--- src/devign.jsx
+++ src/devign.jsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return <h1>some kewl logo will go here</h1>
+    return (
+        <div style={AppStyle}>
+        </div>
+    )
   }
+}
+
+var AppStyle = {
+    display: 'flex',
+    height: '100vh',
 }
 
 ReactDOM.render(<App/>, document.getElementById('app-view'));
