4e21e8681470137ef48915d38fefdacd77ca4594
Tony Rizko <tony@rizkocircle.com>
2016-06-27 02:51:13 -0700

    Rename files and reorganize

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+var AppStyle = {
+    display: 'flex',
+    height: '100vh',
+}
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div style={AppStyle}>
+            </div>
+        );
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-import App from './devign.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById('app-view'));
